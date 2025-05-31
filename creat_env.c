6e3397800aac0b8d4c59235e9d2a76c20fd02b7f
spinoza03 <allaliilyas4@gmail.com>
2025-05-31 18:22:33 +0100

    new

--- creat_env.c
+++ creat_env.c
@@ -6,11 +6,62 @@
 /*   By: ilallali <ilallali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 16:44:32 by ilallali          #+#    #+#             */
-/*   Updated: 2025/05/31 18:08:39 by ilallali         ###   ########.fr       */
+/*   Updated: 2025/05/31 18:21:59 by ilallali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
+
+int parse_env(const char *env_string, char **key_ptr, char **value_ptr)
+{
+    char    *equal_sign_pos;
+    size_t  key_len;
+
+    *key_ptr = NULL;
+    *value_ptr = NULL;
+    if (!env_string || !key_ptr || !value_ptr)
+        return (0); 
+    equal_sign_pos = strchr(env_string, '=');
+    if (equal_sign_pos == NULL)
+    {
+        key_len = strlen(env_string);
+        *key_ptr = (char *)malloc(key_len + 1);
+        if (!(*key_ptr))
+        {
+            perror("minishell: malloc for key (no '=')");
+            return (0);
+        }
+        strcpy(*key_ptr, env_string);
+        *value_ptr = strdup("");
+        if (!(*value_ptr))
+        {
+            perror("minishell: malloc for empty value (no '=')");
+            free(*key_ptr);
+            *key_ptr = NULL;
+            return (0);
+        }
+        return (1);
+    key_len = equal_sign_pos - env_string; 
+    *key_ptr = (char *)malloc(key_len + 1);
+    if (!(*key_ptr))
+    {
+        perror("minishell: malloc for key");
+        return (0);
+    }
+    strncpy(*key_ptr, env_string, key_len);
+    (*key_ptr)[key_len] = '\0';
+    *value_ptr = (char *)malloc(strlen(equal_sign_pos + 1) + 1);
+    if (!(*value_ptr))
+    {
+        perror("minishell: malloc for value");
+        free(*key_ptr);
+        *key_ptr = NULL;
+        return (0);
+    }
+    strcpy(*value_ptr, equal_sign_pos + 1);
+    return (1);
+	}
+}
 
 void create_env_list(t_env_copy **list_head, char **env)
 {
